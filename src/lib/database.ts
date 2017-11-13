841a17afcae41bbfccf54cf6df1d025c6a48f3e2
DBSalvia <dbsalvia@gmail.com>
2017-11-13 20:29:58 +0000

    added logger to database.ts

---
+++ src/lib/database.ts
@@ -0,0 +1,19 @@
+import * as Knex from 'knex';
+
+import { logger } from './logger';
+
+export const database = Knex({
+    client: 'mysql',
+    acquireConnectionTimeout: 10000,
+    connection: {
+        host : process.env.DB_HOST,
+        user : process.env.DB_USER,
+        password : process.env.DB_PASS,
+        database : 'pt_api'
+    }
+});
+
+database.raw('select 1+1 as result').catch(error => {
+    logger.error(error);
+    process.exit(1);
+});
