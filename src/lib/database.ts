280b0633f5be383b209b9d5b797fa46c268a7686
DBSalvia <dbsalvia@gmail.com>
2017-11-14 10:10:44 +0000

    actually resolved last merge conflict. doh.

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,10 +1,7 @@
 import * as Knex from 'knex';
 
-<<<<<<< HEAD
 import { logger } from './logger';
 
-=======
->>>>>>> f720355405c13b426a913242b93f72461c28c592
 export const database = Knex({
     client: 'mysql',
     acquireConnectionTimeout: 10000,
@@ -16,12 +13,7 @@ export const database = Knex({
     }
 });
 
-<<<<<<< HEAD
 database.raw('select 1+1 as result').catch(error => {
     logger.error(error);
-=======
-database.raw('select 1+1 as result').catch(err => {
-    console.log(err);
->>>>>>> f720355405c13b426a913242b93f72461c28c592
     process.exit(1);
 });
