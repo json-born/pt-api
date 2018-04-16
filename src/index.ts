e07fe0b772c30a4883abce7d02dd77bdb54e06cd
DBSalvia <dbsalvia@gmail.com>
2018-04-16 23:18:14 +0100

    closes #27

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as Koa from 'koa';
 import * as Logger from 'koa-logger';
 import * as BodyParser from 'koa-bodyparser';
 import * as dotenv from 'dotenv';
+import * as helmet from 'koa-helmet';
 
 import { router } from './router';
 
@@ -11,6 +12,8 @@ if(process.env.NODE_ENV !== 'test') {
     app.use(Logger());
 }
 
+app.use(helmet());
 app.use(BodyParser());
+
 app.use(router.routes());
 app.use(router.allowedMethods());
